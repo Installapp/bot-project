3be32d4934aa3789de0d061b2ee6e6ea52089198
Farzad Ghavidell <165482356+Installapp@users.noreply.github.com>
2025-10-01 14:19:15 +0330

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // Bot configuration
+    botToken: process.env.DISCORD_BOT_TOKEN,
+    
+    // Channel IDs
+    welcomeChannelId: '1411662893635731497',
+    
+    // Server information
+    serverId: '1410884345161322638',
+    
+    // Channel links for DM message
+    rulesChannelId: '1414313035526574091',
+    rolesChannelId: '1415085128971059201'
+};
